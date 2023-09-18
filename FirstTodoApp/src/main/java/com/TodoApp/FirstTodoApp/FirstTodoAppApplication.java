92bcec4b2066a8dbd44638b0d85e8d89091da7c3
Dev <dev@MacBook-Pro-955.local>
2023-09-18 19:07:12 +0500

    create todo app project from start.spring.io

---
+++ FirstTodoApp/src/main/java/com/TodoApp/FirstTodoApp/FirstTodoAppApplication.java
@@ -0,0 +1,12 @@
+package com.TodoApp.FirstTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTodoAppApplication.class, args);
+	}
+}
---
+++ FirstTodoApp/src/test/java/com/TodoApp/FirstTodoApp/FirstTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TodoApp.FirstTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
